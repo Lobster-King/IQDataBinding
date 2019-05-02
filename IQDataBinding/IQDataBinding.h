a188aab941b4e0c6b84ae1397d49c00d537030cd
Lobster-King <zhiwei.geek@gmail.com>
2019-05-02 22:46:10 +0800

    init

---
+++ IQDataBinding/IQDataBinding.h
@@ -0,0 +1,19 @@
+//
+//  IQDataBinding.h
+//  IQDataBinding
+//
+//  Created by lobster on 2019/5/2.
+//  Copyright © 2019 lobster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IQDataBinding.
+FOUNDATION_EXPORT double IQDataBindingVersionNumber;
+
+//! Project version string for IQDataBinding.
+FOUNDATION_EXPORT const unsigned char IQDataBindingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IQDataBinding/PublicHeader.h>
+
+
