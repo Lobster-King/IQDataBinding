5c32f29f217364ea505dfc1790aeca0c435b62fc
Lobster-King <zhiwei.geek@gmail.com>
2019-05-06 19:56:09 +0800

    Update IQDataBinding.h

--- IQDataBinding/IQDataBinding.h
+++ IQDataBinding/IQDataBinding.h
@@ -16,5 +16,9 @@ FOUNDATION_EXPORT double IQDataBindingVersionNumber;
 FOUNDATION_EXPORT const unsigned char IQDataBindingVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <IQDataBinding/PublicHeader.h>
-
+/**
+ TODO LIST:
+ 1.同一view绑定多个model的情况。
+ 2.view变动->更新viewModel。
+ */
 
